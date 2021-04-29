d3edcbf0de08cb357dec3875ed6d63ea3880f1c4
Sakai Izumi <izumisakai@aliyun.com>
2021-04-29 12:16:16 +0800

    做一些完善

--- basic/DataBase.go
+++ basic/DataBase.go
@@ -9,14 +9,7 @@ import (
 
 var DB = new(sql.DB)
 
-const (
-	Host = "47.108.63.126"
-	Port = "3306"
-	Username = "root"
-	Password = "542270191MSzyl"
-	Database = "netease"
-	DriverName = "mysql"
-)
+
 
 func InitDataBase() {
 	//数据库连接格式和jdbc不一样
