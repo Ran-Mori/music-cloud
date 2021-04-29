7031e11d8c76c77402d7ffd2b9bfd875d9a43a72
Sakai Izumi <izumisakai@aliyun.com>
2021-04-29 12:15:11 +0800

    将常量抽出来

---
+++ basic/Const.go
@@ -0,0 +1,13 @@
+package basic
+
+const (
+	Host = "47.108.63.126"
+	Port = "3306"
+	Username = "root"
+	Password = "542270191MSzyl"
+	Database = "netease"
+	DriverName = "mysql"
+	MusicBasePath = "/home/music/"
+	//MusicBasePath = "A:/"
+)
+
