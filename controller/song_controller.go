271835b021ff424744a51b218050eb8441f84084
izumisakai-zy <izumisakai-zy@outlook.com>
2022-04-21 20:35:00 +0800

    feature(backend_go):add support for picture inside of mp3 file

--- controller/song_controller.go
+++ controller/song_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/julienschmidt/httprouter"
 	"io"
 	"log"
@@ -20,7 +21,8 @@ var songDao = new(dao.SongDao)
 func (s *SongController) Router(router *httprouter.Router) {
 	router.GET("/songs", s.queryAll)
 	router.DELETE("/song/:id", s.deleteOneById)
-	router.GET("/song/download/:id/", s.downLoad)
+	router.GET("/song/download/:id/", s.downLoadMusic)
+	router.GET("/song/picture/download/:id/", s.downloadPicture)
 	router.GET("/song/query/:id", s.queryOneById)
 	router.POST("/song/upload", s.upload)
 	router.GET("/songs/like/:keyword", s.queryLike)
@@ -60,6 +62,15 @@ func (s *SongController) deleteOneById(w http.ResponseWriter, r *http.Request, p
 	} else {
 		w.Write([]byte("successful to delete one by id"))
 	}
+
+	songPath := strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5IdStr, ".mp3"}, "")
+	picPaht := strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5IdStr, ".png"}, "")
+	if _, err := os.Stat(songPath); !errors.Is(err, os.ErrNotExist) {
+		os.Remove(songPath)
+	}
+	if _, err := os.Stat(picPaht); !errors.Is(err, os.ErrNotExist) {
+		os.Remove(picPaht)
+	}
 }
 
 func (s *SongController) upload(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -80,7 +91,9 @@ func (s *SongController) upload(w http.ResponseWriter, r *http.Request, _ httpro
 	//然后在做一下拆分获取歌手和歌曲名
 	md5 := basic.GetMd5(file)
 	fileName := strings.ReplaceAll(header.Filename, " ", "")
-	title, artist := basic.GetTitleAndArtist(file)
+	title, artist, picture := basic.GetTitleArtistAndPicture(file)
+	log.Print("size picture = ")
+	log.Println(len(picture))
 	index := strings.Index(fileName, "-")
 	if len(title) == 0 {
 		title = fileName[index+1 : strings.Index(fileName, ".")]
@@ -98,24 +111,31 @@ func (s *SongController) upload(w http.ResponseWriter, r *http.Request, _ httpro
 	}
 
 	//创建云服务硬盘的文件
-	path := strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5}, "")
-	dist, err := os.Create(path)
-	defer dist.Close()
+	mp3Dist, _ := os.Create(strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5, ".mp3"}, ""))
+	defer mp3Dist.Close()
+	_, mp3Err := io.Copy(mp3Dist, file)
 
-	//做一个流复制操作
-	if _, err := io.Copy(dist, file); err != nil {
-		log.Println(err)
+	var picErr = errors.New("")
+	if len(picture) != 0 {
+		picErr = os.WriteFile(strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5, ".png"}, ""), picture, 0644)
+	}
+
+	if mp3Err == nil && picErr == nil {
+		w.Write([]byte("successful to upload"))
+	} else if mp3Err == nil && picErr != nil {
+		w.Write([]byte("save picture file to disk error or no picture"))
+	} else if mp3Err != nil && picErr == nil {
 		w.Write([]byte("save music file to disk error"))
 	} else {
-		w.Write([]byte("successful to upload"))
+		w.Write([]byte("music fail and picture fail"))
 	}
 }
 
-func (s *SongController) downLoad(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (s *SongController) downLoadMusic(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	md5IdStr := params.ByName("id")
 
 	//确定要在硬盘上读取的流文件的路径
-	filePath := strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5IdStr}, "")
+	filePath := strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5IdStr, ".mp3"}, "")
 	//获取磁盘上的文件流
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -125,15 +145,36 @@ func (s *SongController) downLoad(w http.ResponseWriter, r *http.Request, params
 	}
 	defer file.Close()
 
-	//磁盘文件是完整路径如'/home/music/43a0298b5039248ed09324f'
+	//磁盘文件是完整路径如'/home/music/43a0298b5039248ed09324f.mp3'
 	//设置为强制下载
 	w.Header().Set("content-type", "application/force-download")
 	//设置disk缓存，二次听歌不用拉取
 	w.Header().Set("cache-control", "max-age=31536000")
 	//设置下载的歌曲的名字
 	length := len(file.Name())
-	w.Header().Set("content-disposition", strings.Join([]string{"attachment;fileName=", file.Name()[length-32 : len(file.Name())]}, ""))
+	w.Header().Set("content-disposition", strings.Join([]string{"attachment;fileName=", file.Name()[length-36 : len(file.Name())]}, ""))
 	//把磁盘流复制到respond流
+	io.Copy(w, file)
+}
+
+func (s *SongController) downloadPicture(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	md5IdStr := params.ByName("id")
+
+	filePath := strings.Join([]string{basic.GetUserHomeDir(), basic.MusicStorePath, md5IdStr, ".png"}, "")
+	//获取磁盘上的文件流
+	file, err := os.Open(filePath)
+	if err != nil {
+		w.Write([]byte("file not exist"))
+		log.Println(err)
+		return
+	}
+	defer file.Close()
+
+	//磁盘文件是完整路径如'/home/music/43a0298b5039248ed09324f.png'
+	w.Header().Set("content-type", "application/force-download")
+	w.Header().Set("cache-control", "max-age=31536000")
+	length := len(file.Name())
+	w.Header().Set("content-disposition", strings.Join([]string{"attachment;fileName=", file.Name()[length-36 : len(file.Name())]}, ""))
 	io.Copy(w, file)
 }
 
