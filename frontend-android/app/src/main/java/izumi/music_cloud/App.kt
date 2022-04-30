450d87f33655782676a278976c5d1e3bd36d4e4a
IzumiSakai-ZY <izumisakai@aliyun.com>
2022-04-30 17:08:19 +0800

    feature(android-frontend):finish playing page UI

---
+++ frontend-android/app/src/main/java/izumi/music_cloud/App.kt
@@ -0,0 +1,12 @@
+package izumi.music_cloud
+
+import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Fresco.initialize(this)
+    }
+}
